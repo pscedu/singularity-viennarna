3f792eb9715dbe64ae1ecf34a6d06f2ecafa503a
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2020-10-20 09:07:31 -0400

    Initial untested commit

---
+++ 2.4.14/generate_binaries.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+TOOLS=(AnalyseDists
+AnalyseSeqs
+Kinfold
+RNA2Dfold
+RNAaliduplex
+RNAalifold
+RNAcofold
+RNAdistance
+RNAduplex
+RNAeval
+RNAfold
+RNAforester
+RNAheat
+RNAinverse
+RNALalifold
+RNALfold
+RNApaln
+RNApdist
+RNAparconv
+RNAPKplex
+RNAplex
+RNAplfold
+RNAplot
+RNAsnoop
+RNAsubopt
+RNAup)
+
+cat << EOF > template
+#!/bin/bash
+
+if [ ! $(command -v singularity) ]; then
+	module load singularity/3.6.3
+fi
+
+VERSION=2.4.14
+PACKAGE=viennarna
+TOOL=TOOL_NAME
+DIRECTORY=/opt/packages/\$PACKAGE/\$VERSION
+
+singularity exec -B /pylon5 \$DIRECTORY/singularity-\$PACKAGE-\$VERSION.sif \$TOOL "\$@"
+EOF
+
+for TOOL in "${TOOLS[@]}"
+do
+	echo $TOOL
+        cp template $TOOL
+	sed -i "s/TOOL_NAME/$TOOL/g" $TOOL
+	chmod +x $TOOL
+done
+
+rm -f template
---
+++ 2.4.14/pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+singularity pull https://depot.galaxyproject.org/singularity/viennarna:2.4.14--py27heb12742_3
+mv -v viennarna:2.4.14--py27heb12742_3 singularity-viennarna-2.4.14.sif
